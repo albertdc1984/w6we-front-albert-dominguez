8eba901f34dca1f1528de5d2a19c24cf80d30451
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-02-20 19:28:36 +0100

    Create robot page created. Under construction

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import RobotPage from "./pages/RobotPage/RobotPage";
+import CreatePage from "./pages/CreatePage/CreatePage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/robots" element={<MainPage />} />
         <Route path="/" element={<Navigate to="/robots" />} />
         <Route path="/robots/:id" element={<RobotPage />} />
+        <Route path="/robots/create" element={<CreatePage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
---
+++ src/pages/CreatePage/CreatePage.js
@@ -0,0 +1,9 @@
+const CreatePage = () => {
+  return (
+    <>
+      <h1>THE ROBOTS</h1>
+      <h2>CREATE YOUR ROBOT</h2>
+    </>
+  );
+};
+export default CreatePage;
