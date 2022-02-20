55e59d0333a0692feeba219f023ec8a209e17a01
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-02-20 19:09:36 +0100

    Arranged robot detail styles to make the image fit with different widths

--- src/components/Robot/Robot.js
+++ src/components/Robot/Robot.js
@@ -1,10 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import "./Robot.css";
 
 const Robot = ({ robot }) => {
+  const navigate = useNavigate();
+  const detailedRobot = () => {
+    navigate(`/robots/${robot._id}`);
+  };
   return (
     <div className="robot-container">
       <div className="robot-image-container">
-        <img src={robot.image} alt={robot.name} />
+        <img src={robot.image} alt={robot.name} onClick={detailedRobot} />
       </div>
       <h2 className="robot-name">{robot.name}</h2>
       <ul className="robot-attributes">
