6ca2577df260284999c48f12d6a6f7a308023743
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-02-20 15:34:02 +0100

    Robot detail component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
+import RobotDetail from "./components/RobotDetail/RobotDetail";
 
 import { RobotList } from "./components/RobotList/RobotList";
 
+const fakeRobot = {
+  _id: "621145058f8f1a4e0bc56905",
+  image:
+    "https://i0.wp.com/theelectricagora.com/wp-content/uploads/2016/06/astro_boy1.jpg?fit=688%2C712&ssl=1",
+  velocity: 10,
+  resistance: 10,
+  year: 1952,
+  name: "Astroboy",
+};
 function App() {
   return (
     <div className="App">
       <h1>ROBOTS</h1>
       <RobotList />
+      <RobotDetail robot={fakeRobot} />
     </div>
   );
 }
